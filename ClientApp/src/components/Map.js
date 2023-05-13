943f6c29adc6de9bb51c055a3f7b6172fff6fb28
Carlos Eduardo Camey M <careduardo1999@hotmail.com>
2023-05-12 20:48:43 -0600

    integracion googlemaps

--- ClientApp/src/components/Map.js
+++ ClientApp/src/components/Map.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '400px',
+    width: '1000px',
     height: '400px',
 };
 
+
+
 const center = {
-    lat: 37.7749,
-    lng: -122.4194,
+    lat: 14.5040362873799,
+    lng: -90.61560837309347,
 };
 
-const Map = (props) => {
+const MapComponent = () => {
     return (
         <LoadScript googleMapsApiKey="AIzaSyAIw8ETxdDdM0zwzUZLR2PPcGOii_Rvyp0">
-            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
                 <Marker position={center} />
             </GoogleMap>
         </LoadScript>
     );
 };
 
-export default Map;
+export default MapComponent;
--- ClientApp/src/components/ShipFrom.js
+++ ClientApp/src/components/ShipFrom.js
@@ -3,18 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Map from './Map'
 
 function ShipFrom() {
-
-    <div>
+    return (
+    <Container>
 
         <h1>Mapa de Google</h1>
-        <Map
-            containerElement={<div style={{ height: '400px' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-            loadingElement={<p>Cargando</p> }
-        />
+        <Map />
 
-    </div>
-
+    </Container>
+    )
 }
 
 export default ShipFrom;
