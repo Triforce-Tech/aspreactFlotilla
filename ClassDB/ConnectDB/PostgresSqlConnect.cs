79164d68ca444820f979ef7f7f32bc660449f263
HenryG1995 <45490739+HenryG1995@users.noreply.github.com>
2023-05-21 09:38:35 -0600

    cambios

--- ClassDB/ConnectDB/PostgresSqlConnect.cs
+++ ClassDB/ConnectDB/PostgresSqlConnect.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 
 namespace ClassDB.ConnectDB
 {
     internal class PostgresSqlConnect
     {
+      
+        public int execute(string sqlexecute)
+        {
+
+            NpgsqlConnection connection = new NpgsqlConnection(sqlexecute);
+            connection.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand(sqlexecute, connection);
+            NpgsqlDataReader dataReader = command.ExecuteReader();
+            connection.Close();
+            return 1;
+        }
+
+        public DataTable selectquery(string sqlexecute)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(sqlexecute);
+            connection.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand(sqlexecute, connection);
+            NpgsqlDataReader dataReader = command.ExecuteReader();
+            connection.Close();
+
+            DataTable dt =new DataTable();
+
+
+            return dt;
+        }
+
+
+
     }
 }
